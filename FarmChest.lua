2bfc671554d1c289672d1ccbc7a7b003ebfc9305
Dev-Hinishi <hncbeo@gmail.com>
2026-07-03 20:26:29 +0700

    Implement Discord notification and clipboard functionality
    Added a function to send notifications and set clipboard with Discord link.

--- FarmChest.lua
+++ FarmChest.lua
@@ -1,1 +1,6 @@
-https://discord.gg/9FvuPEpHMZ
+spawn(function()
+while wait(1) do
+pcall(function() setclipboard("https://discord.gg/9FvuPEpHMZ") end) game:GetService("StarterGui"):SetCore("SendNotification",{Title="Join",Text="Discord Community",Duration=1})
+game:GetService("StarterGui"):SetCore("SendNotification",{Title="Get New Script At:",Text="Discord Community",Duration=1})
+game:GetService("StarterGui"):SetCore("SendNotification",{Title="Paste link into Google!",Text="=(((",Duration=1})
+ end end)
